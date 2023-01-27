e0b809ab15d2722d80f76b6b4351af416470d3c8
Specklex <lundjaden@gmail.com>
2023-01-26 22:16:36 -0600

    Issues with S logo path

--- portfolio/src/components/Home/index.js
+++ portfolio/src/components/Home/index.js
@@ -4,6 +4,7 @@ import { useState, useEffect } from 'react';
 import Sidebar from '../Sidebar';
 import './index.scss';
 import AnimatedLetters from '../AnimatedLetters';
+import Logo from '../Logo';
 
 const Home = () => {
     const [letterClass, setLetterClass] = useState('text-animate')
@@ -36,6 +37,7 @@ const Home = () => {
                     <h2>Frontend Developer / JavaScript Expert / Youtuber</h2>
                     <Link to='/contact' className='flat-button'>CONTACT ME</Link>
                 </div>
+                <Logo />
             </div>
         </>
     )
